274495f2fbc5cae4f49e7e0ec14ce68a14296cc4
pingping <77882276+pingpinger-ing@users.noreply.github.com>
2021-02-26 07:50:15 +0000

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 #include <pthread.h>
 #include <thread>
+using namespace std;
 int main() {
 	long times = 500000000;
 	measure([times]() {           //双线程
 		long ans1 = 0, ans2 = 0;
-		thread t1 = thread(NumberAdd, 0, times / 2, ref(ans1));
-		thread t2 = thread(NumberAdd, times / 2, times, ref(ans2));
+		std:：thread t1 = thread(NumberAdd, 0, times / 2, ref(ans1));
+		std:：thread t2 = thread(NumberAdd, times / 2, times, ref(ans2));
 		t1.join();
 		t2.join();
 		cout << "result of two treads: " << ans1 + ans2 << endl;

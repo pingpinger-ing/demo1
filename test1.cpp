15eab96a2dc2cd019a437e26b4c6e81dbbb30096
pingping <77882276+pingpinger-ing@users.noreply.github.com>
2021-02-26 08:48:45 +0000

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,18 +1,23 @@
 #include <iostream> 
 #include <chrono> 
 #include <ctime> 
-#define N 900000000 
+#define N 90000000 
 using namespace std; 
-int main(){ long long sum = 0; 
+int main()
+{ 
+	   long long sum = 0; 
 	   auto start = std::chrono::system_clock::now(); 
 	   std::time_t start_time = std::chrono::system_clock::to_time_t(start); 
-	   cout << "开始时间: " << std::ctime(&start_time); 
-	   for(long long i = 1; i <= N; ++i){ sum += i; } 
+	   cout << "start time: " << std::ctime(&start_time); 
+	   for(long long i = 1; i <= N; ++i)
+	   { 
+		   sum += i; 
+	   } 
 	   auto end = std::chrono::system_clock::now(); 
 	   std::chrono::duration<double> elapsed_seconds = end-start; 
 	   std::time_t end_time = std::chrono::system_clock::to_time_t(end); 
-	   cout << "sum = " << sum << endl; cout << "结束时间: " << std::ctime(&end_time); 
-	   cout << "用 时: " << elapsed_seconds.count() << "s" << endl; return 0; 
+	   cout << "sum = " << sum << endl; cout << "end time: " << std::ctime(&end_time); 
+	   cout << "time cost: " << elapsed_seconds.count() << "s" << endl; return 0; 
 	  }
 
 
